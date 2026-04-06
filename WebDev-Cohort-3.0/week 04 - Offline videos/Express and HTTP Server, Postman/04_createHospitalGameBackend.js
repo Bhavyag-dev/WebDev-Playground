43d6e6c529fa8a3540aca7dfb2e0407365fa9ac1
Bhavya Goyal <gbhavyawork@gmail.com>
2026-04-06 21:15:43 +0530

    learning

--- WebDev-Cohort-3.0/week 04 - Offline videos/Express and HTTP Server, Postman/04_createHospitalGameBackend.js
+++ WebDev-Cohort-3.0/week 04 - Offline videos/Express and HTTP Server, Postman/04_createHospitalGameBackend.js
@@ -15,7 +15,13 @@ DELETE - User can remove a kidney
 
 // import express module using require function and store it in express variable
 const express = require("express");
+
+// create an express application using express function
 const app = express();
+
+// Middleware to parse JSON data in the request body
+app.use(express.json());
+
 
 const users = [{
   name: "bhavya",
@@ -47,4 +53,57 @@ app.get("/", function(req, res){
   })
 })
 
-app.listen(3000);
+/**
+ * Create a route handler for POST request
+ * URL: http://localhost:3000/
+ * 
+ *  Request Body - Send JSON data in the request body to add a new kidney to the user 
+    {
+       "isHealthy": true
+    }
+
+ * Add a new kidney for the user with the health status provided in the request body
+ */
+
+app.post("/", function(req, res) {
+  const isHealthy = req.body.isHealthy;
+      // add a new kidney to the first user in the users array with the isHealthy value from the request body
+  users[0].kidneys.push({
+    healthy:isHealthy,
+  });
+
+  // send the response in JSON format with a message that the "kidney added successfully
+  res.json({
+    message: "Kidney added successfully",
+  });
+});
+
+/**
+ * Create a route handler for PUT request
+ * URL: http://localhost:3000/
+ *
+ * Update all the unhealthy kidney to healthy kidney, if there are no unhealthy kidney then return a 411 status code
+ */
+
+app.put("./", function (req, res) {
+  for (let i=0; i<users[0].kidneys.length; i++) {
+    user[0].kidneys[i].healthy = true;
+  }
+
+  res.json({
+    message: "Kidney replaced successfully",
+  });
+})
+
+/**
+ * Create a route handler for DELETE request
+ * URL: http://localhost:3000/
+ *
+ * Remove all the unhealthy kidney, if there are no unhealthy kidney then return a 411 status code
+ */
+
+app.delete("./", function(req, res){
+  
+})
+
+app.listen(3000);  
