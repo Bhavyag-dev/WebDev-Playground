191cd6f061416c44ad7cb5967417208886fd125a
Bhavya Goyal <gbhavyawork@gmail.com>
2026-07-16 14:59:29 +0530

    Pivoting to a new machine

--- WebDev-Cohort-3.0/Week 08 - course Selling App/8.1 - Backend of  Course Selling App/index.js
+++ WebDev-Cohort-3.0/Week 08 - course Selling App/8.1 - Backend of  Course Selling App/index.js
@@ -10,6 +10,18 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/admin ", adminRouter);
 app.use("/api/v1/course", courseRouter);
 
+async function main() {
+  try{
+  const connection = await mongoose.connect("mongodb+srv://admin_gbhavyawork:%40Bha179vya@cluster0.j2xw4ka.mongodb.net/todo-app-database");
+  console.log("Conneted to database");
+  } catch  (error) {
+    m 
+  }
+} 
+
+
+
+
 app.listen(3000, () => {
   console.log("server is running on port 3000")
 });
